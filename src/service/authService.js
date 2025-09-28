b89af1e163c8df19ef732a1afc5f78c4232f4cb4
vedantpatil <vedantpatil2002@gmail.com>
2025-09-28 14:49:35 +0530

    token update

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,24 +1,54 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const API_URL = "http://localhost:8080/api";
 
-export const registerUser = async (data) => {
-    try {
-        const response = await axios.post(
-            API_URL+"/register",
-            data
-          );
-        return response
-    } catch (error) {
-        throw error;
+// Create a single axios instance
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+// Attach token automatically to all requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Handle expired token globally
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Clear expired token
+      localStorage.removeItem("token");
+      toast.error("Session expired. Please login again.");
+      // Redirect to login page
+      window.location.href = "/login";
     }
-}
+    return Promise.reject(error);
+  }
+);
+
+// Export functions using the axios instance
+export const registerUser = async (data) => {
+  try {
+    const response = await api.post("/register", data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const login = async (data) => {
-    try {
-        const response = await axios.post(API_URL+"/login", data);
-        return response;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const response = await api.post("/login", data);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default api;
